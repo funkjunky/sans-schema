4e8ec717a9bcf70a006c91563543f76223866f04
Jason <jason.mccarrell@logmein.com>
2019-12-15 14:33:27 -0500

    got tests running again

--- src/mergeNormalizedModels.js
+++ src/mergeNormalizedModels.js
@@ -1,6 +1,8 @@
 export const MERGE_NORMALIZED_MODELS = 'sans-schema/MERGE_NORMALIZED_MODELS';
 
-export default mergeNormalizedModels = models => ({
+const mergeNormalizedModels = models => ({
   type: MERGE_NORMALIZED_MODELS,
   models,
 })
+
+export default mergeNormalizedModels;
